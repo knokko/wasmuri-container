0eb8befc1c8409f0f2122c399ef6fa17e8648bf1
knokko <knokogator@hotmail.com>
2020-02-26 21:14:42 +0100

    Use the new WeakMetaVec to reduce the lines of code for MouseManager

--- src/layer/simple/mouselistening.rs
+++ src/layer/simple/mouselistening.rs
@@ -3,14 +3,7 @@ use std::rc::Weak;
 
 use crate::*;
 
-use wasmuri_core::{Region,WeakVec};
-
-struct RegionHandle {
-
-    behavior: Weak<RefCell<dyn ComponentBehavior>>,
-
-    region: Region
-}
+use wasmuri_core::*;
 
 struct FullHandle {
 
@@ -19,48 +12,40 @@ struct FullHandle {
     priority: i8
 }
 
-type AreaClickHandle = RegionHandle;
-
-type AreaScrollHandle = RegionHandle;
-
 type FullScrollHandle = FullHandle;
-
-type AreaMoveHandle = RegionHandle;
-
-type InOutMoveHandle = RegionHandle;
 
 pub struct MouseManager {
 
-    area_click_listeners: Vec<AreaClickHandle>,
+    area_click_listeners: WeakMetaVec<dyn ComponentBehavior, Region>,
     full_click_listeners: WeakVec<dyn ComponentBehavior>,
 
-    area_scroll_listeners: Vec<AreaScrollHandle>,
-    full_scroll_listeners: Vec<FullScrollHandle>,
+    area_scroll_listeners: WeakMetaVec<dyn ComponentBehavior, Region>,
+    full_scroll_listeners: Vec<FullHandle>,
 
-    area_move_listeners: Vec<AreaMoveHandle>,
+    area_move_listeners: WeakMetaVec<dyn ComponentBehavior, Region>,
     full_move_listeners: WeakVec<dyn ComponentBehavior>,
-    in_out_move_listeners: Vec<InOutMoveHandle>
+    in_out_move_listeners: WeakMetaVec<dyn ComponentBehavior, Region>
 }
 
 impl MouseManager {
 
     pub fn new() -> MouseManager {
         MouseManager {
-            area_click_listeners: Vec::new(),
+            area_click_listeners: WeakMetaVec::new(),
             full_click_listeners: WeakVec::new(),
 
-            area_scroll_listeners: Vec::new(),
+            area_scroll_listeners: WeakMetaVec::new(),
             full_scroll_listeners: Vec::new(),
 
-            area_move_listeners: Vec::new(),
+            area_move_listeners: WeakMetaVec::new(),
             full_move_listeners: WeakVec::new(),
-            in_out_move_listeners: Vec::new()
+            in_out_move_listeners: WeakMetaVec::new()
         }
     }
 
     pub fn can_claim_click_space(&self, region: Region) -> bool {
-        for handle in &self.area_click_listeners {
-            if handle.region.intersects_with(region) {
+        for handle in &self.area_click_listeners.vec {
+            if handle.metadata.intersects_with(region) {
                 return false;
             }
         }
@@ -69,8 +54,8 @@ impl MouseManager {
     }
 
     pub fn can_claim_scroll_space(&self, region: Region) -> bool {
-        for handle in &self.area_scroll_listeners {
-            if handle.region.intersects_with(region) {
+        for handle in &self.area_scroll_listeners.vec {
+            if handle.metadata.intersects_with(region) {
                 return false;
             }
         }
@@ -80,25 +65,16 @@ impl MouseManager {
 
     /// Should only be used after can_claim_scroll_space confirmed that this is allowed
     pub fn add_scroll_space_listener(&mut self, behavior: Weak<RefCell<dyn ComponentBehavior>>, region: Region){
-        self.area_scroll_listeners.push(AreaScrollHandle {
-            behavior,
-            region
-        });
+        self.area_scroll_listeners.push(behavior, region);
     }
 
     pub fn add_move_space_listener(&mut self, behavior: Weak<RefCell<dyn ComponentBehavior>>, region: Region){
-        self.area_move_listeners.push(AreaMoveHandle {
-            behavior,
-            region
-        });
+        self.area_move_listeners.push(behavior, region);
     }
 
     /// Should only be used after can_claim_click_space confirmed that this is allowed
     pub fn add_click_space_listener(&mut self, behavior: Weak<RefCell<dyn ComponentBehavior>>, region: Region){
-        self.area_click_listeners.push(AreaClickHandle {
-            behavior,
-            region
-        });
+        self.area_click_listeners.push(behavior, region);
     }
 
     pub fn add_full_click_listener(&mut self, behavior: Weak<RefCell<dyn ComponentBehavior>>){
@@ -114,10 +90,7 @@ impl MouseManager {
     }
 
     pub fn add_in_out_move_listener(&mut self, behavior: Weak<RefCell<dyn ComponentBehavior>>, region: Region){
-        self.in_out_move_listeners.push(InOutMoveHandle {
-            behavior,
-            region
-        });
+        self.in_out_move_listeners.push(behavior, region);
     }
 
     fn add_full_listener(list: &mut Vec<FullHandle>, behavior: Weak<RefCell<dyn ComponentBehavior>>, priority: i8){
@@ -138,32 +111,21 @@ impl MouseManager {
         });
     }
 
-    fn mouse_inside(handle: &RegionHandle, mouse_pos: Option<(f32, f32)>) -> bool {
-        mouse_pos.is_some() && handle.region.is_float_inside(mouse_pos.unwrap())
+    fn mouse_inside(region: Region, mouse_pos: Option<(f32, f32)>) -> bool {
+        mouse_pos.is_some() && region.is_float_inside(mouse_pos.unwrap())
     }
 
     pub fn fire_mouse_move(&mut self, prev_mouse_pos: Option<(f32,f32)>, next_mouse_pos: Option<(f32, f32)>, manager: &ContainerManager) {
-
-        self.in_out_move_listeners.drain_filter(|handle| {
-            match handle.behavior.upgrade() {
-                Some(component_cell) => {
-                    if Self::mouse_inside(handle, prev_mouse_pos) != Self::mouse_inside(handle, next_mouse_pos) {
-                        component_cell.borrow_mut().mouse_move(&mut MouseMoveParams::new(prev_mouse_pos, next_mouse_pos, manager));
-                    }
-                    false
-                }, None => true
-            }
+        self.in_out_move_listeners.for_each_mut(|behavior, region| {
+            if Self::mouse_inside(*region, prev_mouse_pos) != Self::mouse_inside(*region, next_mouse_pos) {
+                behavior.mouse_move(&mut MouseMoveParams::new(prev_mouse_pos, next_mouse_pos, manager));
+            } false
         });
 
-        self.area_move_listeners.drain_filter(|handle| {
-            match handle.behavior.upgrade() {
-                Some(component_cell) => {
-                    if Self::mouse_inside(handle, prev_mouse_pos) || Self::mouse_inside(handle, next_mouse_pos) {
-                        component_cell.borrow_mut().mouse_move(&mut MouseMoveParams::new(prev_mouse_pos, next_mouse_pos, manager));
-                    }
-                    false
-                }, None => true
-            }
+        self.area_move_listeners.for_each_mut(|behavior, region| {
+            if Self::mouse_inside(*region, prev_mouse_pos) || Self::mouse_inside(*region, next_mouse_pos) {
+                behavior.mouse_move(&mut MouseMoveParams::new(prev_mouse_pos, next_mouse_pos, manager));
+            } false
         });
 
         self.full_move_listeners.for_each_mut(|behavior| {
@@ -173,18 +135,12 @@ impl MouseManager {
     }
 
     pub fn fire_mouse_click(&mut self, manager: &ContainerManager, mouse_pos: (f32,f32), click: ClickInfo) {
-
-        self.area_click_listeners.drain_filter(|handle| {
-            match handle.behavior.upgrade() {
-                Some(component_cell) => {
-                    if handle.region.is_float_inside(mouse_pos) {
-                        component_cell.borrow_mut().mouse_click_inside(&mut MouseClickParams::new(mouse_pos, click, manager));
-                    } else {
-                        component_cell.borrow_mut().mouse_click_outside(&mut MouseClickOutParams::new(click, manager));
-                    }
-                    false
-                }, None => true
-            }
+        self.area_click_listeners.for_each_mut(|behavior, region| {
+            if region.is_float_inside(mouse_pos) {
+                behavior.mouse_click_inside(&mut MouseClickParams::new(mouse_pos, click, manager));
+            } else {
+                behavior.mouse_click_outside(&mut MouseClickOutParams::new(click, manager));
+            } false
         });
 
         self.full_click_listeners.for_each_mut(|behavior| {
@@ -194,13 +150,9 @@ impl MouseManager {
     }
 
     pub fn fire_mouse_click_outside(&mut self, manager: &ContainerManager, click: ClickInfo) {
-        self.area_click_listeners.drain_filter(|handle| {
-            match handle.behavior.upgrade() {
-                Some(component_cell) => {
-                    component_cell.borrow_mut().mouse_click_outside(&mut MouseClickOutParams::new(click, manager));
-                    false
-                }, None => true
-            }
+        self.area_click_listeners.for_each_mut(|behavior, _region| {
+            behavior.mouse_click_outside(&mut MouseClickOutParams::new(click, manager));
+            false
         });
 
         self.full_click_listeners.for_each_mut(|behavior| {
@@ -214,15 +166,10 @@ impl MouseManager {
         let mut consumed = false;
 
         if mouse_pos.is_some() {
-            self.area_scroll_listeners.drain_filter(|handle| {
-                match handle.behavior.upgrade() {
-                    Some(component_cell) => {
-                        if !consumed && handle.region.is_float_inside(mouse_pos.unwrap()){
-                            consumed = component_cell.borrow_mut().mouse_scroll(&mut MouseScrollParams::new(mouse_pos, delta, manager));
-                        }
-                        false
-                    }, None => true
-                }
+            self.area_scroll_listeners.for_each_mut(|behavior, region| {
+                if !consumed && region.is_float_inside(mouse_pos.unwrap()){
+                    consumed = behavior.mouse_scroll(&mut MouseScrollParams::new(mouse_pos, delta, manager));
+                } false
             });
         }
 
